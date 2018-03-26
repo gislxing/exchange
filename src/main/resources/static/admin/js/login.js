e72b71828ec2664631fe4da3a8430c29a1d7f9fe
gislxing <zbs1016@163.com>
2018-03-26 15:53:19 +0800

    登陆页面添加回车触发登陆请求

--- src/main/resources/static/admin/js/login.js
+++ src/main/resources/static/admin/js/login.js
@@ -1,37 +1,28 @@
+$(function () {
+    $("body").keyup(function (event) {
+        if (event.keyCode == 13) {
+            submitButton();
+        }
+    });
+});
 
-function changeValiCode(dom){
+function changeValiCode(dom) {
     var img = $(dom).prev("img");
     img.attr("src", "/admin/valicode?" + Math.random());
     img.click;
 }
-function submitButton(){
-    /*var imageCode = $("#imageCode").val();
-    $.post("/admin/loginAdmin",
-        {
-            "imageCode": imageCode,
-            "username": imageCode,
-            "password": password
-        },
-        function(data){
-            if(data.flag == "success"){//登录成功
-                layer.msg(data.msg,{icon:1,time:1000});
-                location.href ="/admin/index";
-            }else{
-                layer.msg(data.msg,{icon:1,time:1000});
-            }
-        }, "json");*/
 
+function submitButton() {
     $("#form-admin-login").ajaxSubmit({
         type: 'post',
         url: "/admin/loginAdmin",
         success: function (data) {
-            if(data.flag == "success"){//登录成功
-                layer.msg(data.msg,{icon:1,time:1000});
-                location.href ="/admin/index";
-            }else{
-                layer.msg(data.msg,{icon:1,time:1000});
+            if (data.flag == "success") {//登录成功
+                layer.msg(data.msg, {icon: 1, time: 1000});
+                location.href = "/admin/index";
+            } else {
+                layer.msg(data.msg, {icon: 1, time: 1000});
             }
         }
     });
-
 }
